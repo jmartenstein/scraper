2edbf80a4ce4c0aee0a78203fc5ac5b5dfe0140d
Justin Martenstein <jmartenstein@gmail.com>
2011-06-05 09:14:58 -0700

    Fix formatting in ruby scraper

--- scraper.rb
+++ scraper.rb
@@ -44,76 +44,76 @@ site_parser = {
 
 def parse(hash, name)
 
-	# grab the url for what the site's url, but timeout if the script
-	# waits too long
-	begin
-		@doc = Nokogiri::HTML(open(hash['url'], :read_timeout => 5))
-	rescue Timeout::Error
-		abort "timeout!"
-	end
+   # grab the url for what the site's url, but timeout if the script
+   # waits too long
+   begin
+      @doc = Nokogiri::HTML(open(hash['url'], :read_timeout => 5))
+   rescue Timeout::Error
+      abort "timeout!"
+   end
 
-	# check to make sure there are sub nodes
-	sub_nodes = hash['sub_nodes']
-	if sub_nodes.nil?
-		abort "no sub nodes found!"
-	end
+   # check to make sure there are sub nodes
+   sub_nodes = hash['sub_nodes']
+   if sub_nodes.nil?
+      abort "no sub nodes found!"
+   end
 
-	# initialize the hash to store all the values / strings retrieved
-	# from the html document, and a few other housekeeping elements
-	list_list 			= []
-	last_key_count 	= 0
-	i 						= 0
+   # initialize the hash to store all the values / strings retrieved
+   # from the html document, and a few other housekeeping elements
+   list_list         = []
+   last_key_count    = 0
+   i                 = 0
 
-	# extract each of the sub nodes
-	sub_nodes.each do | path_list |
+   # extract each of the sub nodes
+   sub_nodes.each do | path_list |
 
-		# if we actually want the second layer "node" to operate on a subset
-		# of the document, then we will probably need to change the iterator
-		# to some sort of recursive function
-		@node = @doc
+      # if we actually want the second layer "node" to operate on a subset
+      # of the document, then we will probably need to change the iterator
+      # to some sort of recursive function
+      @node = @doc
 
-		path_list.keys.each do | key |
+      path_list.keys.each do | key |
 
-			path = path_list[key]
+         path = path_list[key]
 
-			# initialize a list to store each of the (string) values
-			value_list = []
+         # initialize a list to store each of the (string) values
+         value_list = []
 
-			if key == 'xpath'
-				# do an xpath search for each of our nodes
-				@node.root.xpath(path).each do | element |
-					value_list.push(element.text)
-				end
-			end
+         if key == 'xpath'
+            # do an xpath search for each of our nodes
+            @node.root.xpath(path).each do | element |
+               value_list.push(element.text)
+            end
+         end
 
-			# now store the list back into a (new) hash
-			list_list.push(value_list)
+         # now store the list back into a (new) hash
+         list_list.push(value_list)
 
-			# check to see if the current list is larger than the last one
-			# we checked; if it, throw a warning
-			if i > 0
-				if last_key_count > value_list.count
-					$stderr.puts "WARNING: node count mismatch"
-					$stderr.puts "current  count: value_list.count"
-					$stderr.puts "previous count: #{last_key_count}"
-				end
-			end
+         # check to see if the current list is larger than the last one
+         # we checked; if it, throw a warning
+         if i > 0
+            if last_key_count > value_list.count
+               $stderr.puts "WARNING: node count mismatch"
+               $stderr.puts "current  count: value_list.count"
+               $stderr.puts "previous count: #{last_key_count}"
+            end
+         end
 
-			# increment our counts
-			last_key_count = value_list.count
-			i = i + 1
+         # increment our counts
+         last_key_count = value_list.count
+         i = i + 1
 
-		end
+      end
 
-	end
+   end
 
-	(0 ... last_key_count).each do | i |
-		csv_list = [name]
-		list_list.each do | list |
-			csv_list.push(list[i])
-		end
-		puts csv_list.join(",")
-	end
+   (0 ... last_key_count).each do | i |
+      csv_list = [name]
+      list_list.each do | list |
+         csv_list.push(list[i])
+      end
+      puts csv_list.join(",")
+   end
 
 end  # parse
 
